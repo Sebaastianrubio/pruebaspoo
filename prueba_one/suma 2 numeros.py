f8c613427fa756aad3a2f0ba7f8a8d1093eff37b
Sebaastianrubio <as.rubiof@uea.edu.ec>
2024-08-06 13:40:03 -0500

    1

---
+++ prueba_one/suma 2 numeros.py
@@ -0,0 +1,30 @@
+#suma de 2 numeros enteros positivos
+a=60
+b=30
+suma=a+b
+print("el resultado de la suma es:",suma)
+
+#suma de 2 numeros enteros
+n1=int(input("ingrese el numero:"))
+n2=int(input("ingrese el segundo  numero:"))
+suma=n1+n2
+print("la suma es:",suma)
+
+#suma de numeros decimales
+n1=float(input("ingrese el numero"))
+n2=float(input("ingrese el segundo numero"))
+suma=n1+n2
+print("la suma es:",suma)
+
+
+#programa que suma 2 numeros ingresados por el ususario
+num1=0
+num2=0
+suma=0
+print("Bienvenido usuario")
+print("ingrese un numero entero")
+num1=int(input())
+print("ingrese otro numero entero")
+num2=int(input())
+suma=num1+num2
+print("el resultado de la suma es:",suma)
